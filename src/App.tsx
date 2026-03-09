4c95d4e3d150b93b0cb7150cc651b849a0ff037a
preethima26 <preethithiruselvam26@gmail.com>
2026-03-09 15:49:27 +0530

    Add test comment for GitHub check

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Test comment: GitHub push check
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
